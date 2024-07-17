4d359783e8c41cf8f9f04fc7d61eaf9f9bed5ec2
Benjamin Corion <tabencor@gmail.com>
2024-07-17 16:25:08 +0000

    new functions

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -7,6 +7,7 @@ SELECT
     , CAST(purchase_price AS FLOAT64) AS purchase_price
     , ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost
     , ROUND(s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2), 2) AS margin
+    , {{ margin_percent('s.revenue', 's.quantity*CAST(p.purchase_price AS FLOAT64)') }} AS margin_percent
 FROM {{ref("stg_gz_raw_data__sales")}} s
 LEFT JOIN {{ref("stg_gz_raw_data__product")}} p 
 USING (products_id)
