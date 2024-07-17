dad8e311b57b38efc7e4034982d833c29968559d
Benjamin Corion <tabencor@gmail.com>
2024-07-17 16:17:30 +0000

    finance_campaigns_day build

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,15 @@
+SELECT 
+  date_date,
+  operational_margin - ads_cost AS ads_margin,
+  ROUND(average_basket,2) AS average_basket,
+  operational_margin,
+  ads_cost,
+  ads_impression,
+  ads_clicks,
+  nb_transaction,
+  revenue,
+  margin,
+FROM {{ ref('int_campaigns_day') }}
+FULL OUTER JOIN {{ ref('finance_days') }} 
+  USING (date_date)
+ORDER BY date_date DESC
